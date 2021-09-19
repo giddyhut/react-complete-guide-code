44f3646e37fe7666b17b07e7e3c432f3a1658a64
Allen <cicaktech@gmail.com>
2021-09-19 23:39:08 +0800

    Missing updates

--- code/08-splitting-components-into-multiple-components/src/App.js
+++ code/08-splitting-components-into-multiple-components/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
@@ -21,31 +21,18 @@ function App() {
       amount: 450,
       date: new Date(2021, 5, 12),
     },
+    {
+      id: 'e5',
+      title: 'New Moon Mooncakes',
+      amount: 45,
+      date: new Date(2021, 5, 13),
+    }
   ];
 
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expenses expensesList={expenses} />
     </div>
   );
 }
--- code/08-splitting-components-into-multiple-components/src/components/Expenses.js
+++ code/08-splitting-components-into-multiple-components/src/components/Expenses.js
@@ -6,26 +6,13 @@ function Expenses(props) {
 
     return (
         <div>
+        {expenses.map(item => 
             <ExpenseItem
-                title={expenses[0].title}
-                amount={expenses[0].amount}
-                date={expenses[0].date}
+                title={item.title}
+                amount={item.amount}
+                date={item.date}
             />
-            <ExpenseItem
-                title={expenses[1].title}
-                amount={expenses[1].amount}
-                date={expenses[1].date}
-            />
-            <ExpenseItem
-                title={expenses[2].title}
-                amount={expenses[2].amount}
-                date={expenses[2].date}
-            />
-            <ExpenseItem
-                title={expenses[3].title}
-                amount={expenses[3].amount}
-                date={expenses[3].date}
-            />
+        )}
         </div>
     );
 }
